8f42facdd7c0118d98c67293ff7ec72f56a213a5
mahmoud <mbasel2004@gmail.com>
2025-04-26 18:37:01 +0300

    fix

--- public/client.js
+++ public/client.js
@@ -45,6 +45,11 @@ function initPeer(targetId, isInitiator) {
         config: {
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                }
             ]
         },
         sdpTransform: (sdp) => {
